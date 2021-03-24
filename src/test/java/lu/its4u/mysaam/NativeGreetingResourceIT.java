44ad11c888b1c0b3ada724c54542e0e1577bacdd
Arnaud Lalouette <arnaud.lalouette@its4u.com>
2021-03-24 10:03:47 +0100

    Initial version

---
+++ src/test/java/lu/its4u/mysaam/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package lu.its4u.mysaam;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
